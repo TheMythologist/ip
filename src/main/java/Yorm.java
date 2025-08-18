a5895f37c457b148d1bea2a9c35de2dedf11613a
TheMythologist <leekaixuan2001@gmail.com>
2025-08-18 15:36:29 +0800

    Store texts

--- src/main/java/Yorm.java
+++ src/main/java/Yorm.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Yorm {
@@ -7,15 +8,37 @@ public class Yorm {
         System.out.println("Hello! I'm Yorm!");
         System.out.println("What can I do for you?");
 
+        Chatbot bot = new Chatbot();
+
         while (true) {
-            String word = s.next();
-            if (word.equals("bye")) {
+            String string = s.nextLine();
+            if (string.equals("bye")) {
                 break;
+            } else if (string.equals("list")) {
+                bot.printTexts();
+            } else {
+                bot.addText(string);
             }
-            System.out.println(word);
         }
 
         System.out.println("Bye. Hope to see you again soon!");
         s.close();
+    }
+}
+
+class Chatbot {
+    private ArrayList<String> texts = new ArrayList<>();
+
+    public void addText(String string) {
+        this.texts.add(string);
+        System.out.println(String.format("added: %s", string));
+    }
+
+    public void printTexts() {
+        int counter = 1;
+        for (String string : this.texts) {
+            System.out.println(String.format("%d. %s", counter, string));
+            counter++;
+        }
     }
 }
