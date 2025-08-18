3a88b74e8f33811f2a937650655dfe19352021de
TheMythologist <leekaixuan2001@gmail.com>
2025-08-18 15:28:10 +0800

    Repeat commands

--- src/main/java/Yorm.java
+++ src/main/java/Yorm.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Yorm {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
         System.out.println("Hello! I'm Yorm!");
-        System.out.println(" What can I do for you?");
-        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("What can I do for you?");
+
+        while (true) {
+            String word = s.next();
+            if (word.equals("bye")) {
+                break;
+            }
+            System.out.println(word);
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
+        s.close();
     }
 }
